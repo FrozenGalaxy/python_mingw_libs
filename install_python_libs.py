c7a5403e3a88b7ea234586fc98fae1bdfe4b81ee
FrozenGalaxy <156858866+FrozenGalaxy@users.noreply.github.com>
2024-05-29 11:53:10 +0200

    Update install_python_libs.py

--- install_python_libs.py
+++ install_python_libs.py
@@ -1,196 +1,193 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-# #################################################################################################################
-# Copyright (C) 2017 DeadSix27 (https://github.com/DeadSix27/python_mingw_libs)
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# #################################################################################################################
-
-import sys,os,urllib
-from distutils.version import LooseVersion
-from urllib.request import urlretrieve
+import sys
+import os
+import urllib
+import subprocess
+import re
 
 _DEBUG = False
 
-RC_VERS = { '3.6.7' : '3.6.7rc2' }
-
-PACKAGE_STUFF = {
-		'dllzname' : 'python%%SHORT%%',
-		'pc_names' : (
-			'python-%%SHORT_DOT%%.pc',
-			'python3.pc',
-			'python-%%SHORT_DOT%%m.pc',
-		),
-		'libname' : 'libpython%%SHORT%%.a',
-		'pcfile' : 
-			'prefix=%%PREFIX%%'
-			'\nexec_prefix=${prefix}'
-			'\nlibdir=${exec_prefix}/lib'
-			'\nincludedir=${prefix}/include'
-			'\n'
-			'\nName: Python'
-			'\nDescription: Python library'
-			'\nRequires:'
-			'\nVersion: %%VERSION%%'
-			'\nLibs.private:'
-			'\nLibs: -L${libdir} -lpython%%SHORT%%'
-			'\nCflags: -I${includedir}/python3',
-}
-
-def run_cmd(cmd):
-	if _DEBUG:
-		print("\n--Running command in '%s': '%s'\n--" % (os.getcwd(),cmd))
-	if os.system(cmd) != 0:
-		print("Failed to execute: " + str(cmd))
-		exit(1)
-
-def short_version(v,joinStr = ""):
-	return joinStr.join(v.split(".")[0:2])
-
 def is_tool(name):
-	from distutils.spawn import find_executable
-	return find_executable(name) is not None
+    from distutils.spawn import find_executable
+    return find_executable(name) is not None
+
+VS_PC = """prefix=%%PREFIX%% 
+exec_prefix=${prefix} 
+libdir=${exec_prefix}/lib 
+includedir=${prefix}/include/vapoursynth 
+ 
+Name: vapoursynth 
+Description: A frameserver for the 21st century 
+Version: %%VERSION%% 
+ 
+Requires.private: zimg 
+Libs: -L${libdir} -lvapoursynth 
+Libs.private: -L${libdir} -lzimg
+Cflags: -I${includedir}"""
+
+VSS_PC = """prefix=%%PREFIX%% 
+exec_prefix=${prefix} 
+libdir=${exec_prefix}/lib 
+includedir=${prefix}/include/vapoursynth 
+ 
+Name: vapoursynth-script 
+Description: Library for interfacing VapourSynth with Python 
+Version: %%VERSION%%
+ 
+Requires: vapoursynth 
+Requires.private: python-%%PY_VER_DOT%%
+Libs: -L${libdir} -lvapoursynth-script 
+Libs.private: -lpython%%PY_VER%%
+Cflags: -I${includedir}"""
+
+def runCmd(cmd):
+    if _DEBUG:
+        print("\n--Running command in '%s': '%s'\n--" % (os.getcwd(), cmd))
+    if os.system(cmd) != 0:
+        print("Failed to execute: " + str(cmd))
+        exit(1)
 
 def exitHelp():
-	print("install_python_libs.py install/uninstall <arch> <version> <install_prefix> - e.g install_python_libs.py amd64 3.8.2 /test/cross_compilers/....../")
-	exit(1)
-	
-def simplePatch(infile,replacetext,withtext):
-	lines = []
-	print("Patching " + infile )
-	with open(infile) as f:
-		for line in f:
-			line = line.replace(replacetext, withtext)
-			lines.append(line)
-	with open(infile, 'w') as f2:
-		for line in lines:
-			f2.write(line)
+    print("install_vapoursynth_libs.py install/uninstall <64/32> <version> <install_prefix> <dlltool> <gendef> - e.g install_vapoursynth_libs.py 64 R49 /test/cross_compilers/....../ DLLTOOLPATH GENDEFPATH")
+    exit(1)
 
-if not is_tool("rsync"):
-	print("Please make sure that rsync is installed.")
-	exit(1)
-			
+def simplePatch(infile, replacetext, withtext):
+    lines = []
+    print("Patching " + infile)
+    with open(infile) as f:
+        for line in f:
+            line = line.replace(replacetext, withtext)
+            lines.append(line)
+    with open(infile, 'w') as f2:
+        for line in lines:
+            f2.write(line)
+
+def lib_to_a(lib_filename, a_output_name, dlltool):
+    # Extract symbols from the .lib file
+    symbols_txt = "symbols.txt"
+    with open(symbols_txt, "w") as symbols_file:
+        subprocess.run(["llvm-nm", lib_filename], stdout=symbols_file)
+
+    # Parse symbols and create a .def file
+    def_filename = "temp.def"
+    with open(symbols_txt, "r") as f:
+        lines = f.readlines()
+
+    exports = []
+    for line in lines:
+        match = re.match(r'^[0-9a-fA-F]+\s+([a-zA-Z_][a-zA-Z0-9_]*)$', line)
+        if match:
+            exports.append(match.group(1))
+
+    with open(def_filename, "w") as f:
+        f.write(f"LIBRARY {os.path.splitext(lib_filename)[0]}\n")
+        f.write("EXPORTS\n")
+        for symbol in exports:
+            f.write(f"{symbol}\n")
+
+    # Create the .a file using llvm-dlltool
+    subprocess.run([dlltool, "-d", def_filename, "-l", a_output_name])
+
+    # Clean up temporary files
+    os.remove(symbols_txt)
+    os.remove(def_filename)
+
+    print(f"Created {a_output_name} from {lib_filename}")
+
+def check_version(ver_suff):
+    if int(ver_suff) < 58:
+        print("Error: VapourSynth version must be 58 or higher.")
+        exit(1)
+
+if not is_tool("rsync") or not is_tool("7z"):
+    print("Please make sure that p7zip and rsync are installed.")
+    exit(1)
+
 if len(sys.argv) != 7:
-	exitHelp()
+    exitHelp()
 else:
-	if sys.argv[1] == "install":
-		arch    = sys.argv[2]
-		ver     = sys.argv[3]
-		
-		
-		if LooseVersion(ver) <= LooseVersion("3.10"):
-			print("Use newer version than 3.10.")
-			exit()
-		rc_ver  = ver
-		if ver in RC_VERS:
-			rc_ver = RC_VERS[ver]
-		prefix  = sys.argv[4]
-		dlltool = sys.argv[5]
-		gendef  = sys.argv[6]
-		
-		ver_short     = short_version(ver)
-		ver_short_dot = short_version(ver,".")
-			
-		run_cmd("mkdir -p work")
-		run_cmd("mkdir -p bin")
-		os.chdir("work")
-		run_cmd("mkdir -p lib")
-		os.chdir("lib")
-		
-		url,filename = 'https://www.python.org/ftp/python/{0}/python-{2}-embed-{1}.zip'.format(ver,arch,rc_ver), 'python-{0}-embed-{1}.zip'.format(rc_ver,arch)
-		print("Downloading: " + url)
-		urlretrieve(url,filename)
-		print("Done")
-		
-		dllname = PACKAGE_STUFF["dllzname"].replace("%%SHORT%%",ver_short)
-		
-		print("Extracting dll")
-		run_cmd('unzip -po {0} {1}.dll >{1}.dll'.format(filename,dllname))
-		run_cmd('unzip -po {0} libffi-8.dll >libffi-8.dll'.format(filename))
-		run_cmd('unzip -po {0} _ctypes.pyd >_ctypes.pyd'.format(filename))
-		run_cmd('unzip -po {0} {1}.zip >{1}.zip'.format(filename,dllname))
-		
-		print("Local installing dll")
-		run_cmd('cp {0}.zip ../../bin'.format(dllname))
-		run_cmd('cp {0}.dll ../../bin'.format(dllname))
-		run_cmd('cp _ctypes.pyd ../../bin')
-		run_cmd('cp libffi-8.dll ../../bin')
-		print("Done")
-		print("Deleting archive")
-		os.unlink(filename)
-		
-		print("Creating library")
-		run_cmd("{0} {1}.dll".format(gendef,dllname))
-		
-		defname = dllname + ".def"
-		
-		run_cmd(f"{dlltool} -m i386:x86-64 -D {dllname}.dll -d {defname} -l {PACKAGE_STUFF['libname'].replace('%%SHORT%%',ver_short)}")
-		
-		print("Done")
-		
-		os.unlink(defname)
-		os.unlink(dllname+".dll")		
-		
-		run_cmd("mkdir -p pkgconfig")
-		
-		os.chdir("pkgconfig")
-		
-		print("Creating pkgconfig")
-		
-		pc = PACKAGE_STUFF["pcfile"].replace('%%PREFIX%%',prefix).replace('%%VERSION%%',ver).replace("%%SHORT%%",ver_short)
-		
-		for fn in PACKAGE_STUFF["pc_names"]:
-			with open(fn.replace("%%SHORT_DOT%%",ver_short_dot),"w") as f:
-				f.write(pc)
-		
-		os.chdir("..")
-		
-		os.chdir("..")
-		url,filename = 'https://www.python.org/ftp/python/{0}/Python-{1}.tgz'.format(ver,rc_ver), 'Python-{0}.tgz'.format(rc_ver)
-				
-		print("Downloading: " + url)
-		urlretrieve(url,filename)
-		print("Done")
-		print("Extracting headers")
-		run_cmd("mkdir include")
-		run_cmd("tar -xvf {0} Python-{1}/Include".format(filename,rc_ver))
-		run_cmd("mv Python-{0}/Include include/python3".format(rc_ver))
-		
-		
-		run_cmd("tar -xvf {0} Python-{1}/PC/pyconfig.h".format(filename,rc_ver))
-		
-		simplePatch("Python-{0}/PC/pyconfig.h".format(rc_ver),"#define hypot _hypot","#if (__GNUC__<6)\n#define hypot _hypot\n#endif")
-		
-		run_cmd("mv Python-{0}/PC/pyconfig.h include/python3/".format(rc_ver))
-		
-		
-		
-		print("Done")
-		os.unlink(filename)
-		run_cmd("rm -r Python-{0}".format(rc_ver))
-		
-		os.chdir("..")
-		print("Installing to " + prefix)
-		
-		if not os.path.isdir(prefix):
-			print("ERROR: '" + prefix + "' does not exist")
-			run_cmd("rm -r work")
-			exit(1)
-		
-		run_cmd("rsync -aKv work/ {0}".format(prefix))
-		
-		run_cmd("rm -r work")
-		
-		
-	elif sys.argv[1] == "uninstall":
-		pass
-	else:
-		exitHelp()
+    if sys.argv[1] == "install":
+        arch     = sys.argv[2]
+        ver      = sys.argv[3]
+        ver_suff = ver[1:]
+        prefix   = sys.argv[4]
+        dlltool  = sys.argv[5]
+        gendef   = sys.argv[6]
+
+        check_version(ver_suff)
+
+        runCmd("mkdir -p work")
+        runCmd("mkdir -p bin")
+        os.chdir("work")
+        print("Downloading")
+        runCmd("wget https://github.com/vapoursynth/vapoursynth/releases/download/{0}/VapourSynth{1}-Portable-{0}.zip".format(ver, arch))
+        runCmd('7z x -aoa "VapourSynth{1}-Portable-{0}.zip"'.format(ver, arch))
+
+        print("Local installing binaries")
+        runCmd("cp {0} ../bin".format("VSScript.dll"))
+
+        VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%", "3.12").replace("%%PY_VER%%", "312")
+
+        print("Creating library")
+
+        # Convert VapourSynth.lib to libvapoursynth.a
+        lib_to_a("sdk/lib64/VapourSynth.lib", "libvapoursynth.a", dlltool)
+
+        # Create libvapoursynth-script.a from VSScript.dll
+        runCmd("{0} {1}".format(gendef, "VSScript.dll"))
+        runCmd(f"{dlltool} -m i386:x86-64 -D VSScript.dll -d VSScript.def -l libvapoursynth-script.a")
+
+        runCmd("mkdir lib")
+
+        runCmd("mv libvapoursynth.a lib/")
+        runCmd("mv libvapoursynth-script.a lib/")
+
+        os.chdir("lib")
+
+        runCmd("mkdir pkgconfig")
+
+        os.chdir("pkgconfig")
+
+        print("Creating pkgconfig")
+
+        pc_script = VSS_PC.replace('%%PREFIX%%', prefix).replace('%%VERSION%%', ver_suff)
+        pc        = VS_PC.replace('%%PREFIX%%', prefix).replace('%%VERSION%%', ver_suff)
+
+        with open("vapoursynth.pc", "w") as f:
+            f.write(pc)
+
+        with open("vapoursynth-script.pc", "w") as f:
+            f.write(pc_script)
+
+        os.chdir("..")
+        os.chdir("..")
+
+        runCmd("mkdir include")
+        os.chdir("include")
+
+        runCmd("wget https://github.com/vapoursynth/vapoursynth/archive/{0}.tar.gz".format(ver))
+        runCmd("tar -xvf {0}.tar.gz vapoursynth-{0}/include".format(ver))
+
+        runCmd("mv vapoursynth-{0}/include vapoursynth".format(ver))
+        runCmd("rm -r vapoursynth-{0}".format(ver))
+        runCmd("rm {0}.tar.gz".format(ver))
+        os.chdir("..")
+
+        runCmd("mkdir ../work2")
+
+        runCmd("mv include ../work2")
+        runCmd("mv lib ../work2")
+
+        os.chdir("..")
+
+        print("Installing to " + prefix)
+        runCmd("rsync -aKv work2/ {0}".format(prefix))
+
+        runCmd("rm -r work")
+        runCmd("rm -r work2")
+
+    elif sys.argv[1] == "uninstall":
+        pass
+    else:
+        exitHelp()
