ab1ae2821a48d9911c5e59c78395dd0616119c17
FrozenGalaxy <156858866+FrozenGalaxy@users.noreply.github.com>
2026-04-11 22:46:06 +0200

    Comment out pyconfig.h.in related commands
    Comment out commands related to pyconfig.h.in processing.

--- install_python_libs.py
+++ install_python_libs.py
@@ -165,11 +165,11 @@ else:
 		run_cmd("mv Python-{0}/Include include/python3".format(rc_ver))
 		
 		
-		run_cmd("tar -xvf {0} Python-{1}/PC/pyconfig.h.in".format(filename,rc_ver))
+		#run_cmd("tar -xvf {0} Python-{1}/PC/pyconfig.h.in".format(filename,rc_ver))
 		
-		simplePatch("Python-{0}/PC/pyconfig.h.in".format(rc_ver),"#define hypot _hypot","#if (__GNUC__<6)\n#define hypot _hypot\n#endif")
+		#simplePatch("Python-{0}/PC/pyconfig.h.in".format(rc_ver),"#define hypot _hypot","#if (__GNUC__<6)\n#define hypot _hypot\n#endif")
 		
-		run_cmd("mv Python-{0}/PC/pyconfig.h.in include/python3/pyconfig.h".format(rc_ver))
+		#run_cmd("mv Python-{0}/PC/pyconfig.h.in include/python3/pyconfig.h".format(rc_ver))
 		
 		
 		
